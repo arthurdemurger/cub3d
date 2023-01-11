665f99e5c15c79b1ae21e960e635311d3989bf9a
Gilles Poncelet <gilles.ponpon@gmail.com>
2023-01-11 15:18:00 +0100

    MAJ

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
+/*   By: gponcele <gponcele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 11:31:20 by ademurge          #+#    #+#             */
-/*   Updated: 2023/01/11 11:31:55 by ademurge         ###   ########.fr       */
+/*   Updated: 2023/01/11 15:15:37 by gponcele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 # define CUB3D_H
 
-#include <stdio.h>
+# include <stdio.h>
+# include <math.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 #endif
